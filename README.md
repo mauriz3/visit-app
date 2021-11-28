12bcf8056d60148529fe0230c5cbd7574b0c4d91
Maurizio Rendon <maurizio.rendon@system73.com>
2021-11-28 12:55:35 +0000

    feat(FE): Creating FE skeleton and some config changes

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # visit-app
-Django + React visit app
+Django + React visit app.
+
+## Start Frontend
+    cd frontend
+    npm start
+
+## Start Backend
+    cd backend
+    source venv/bin/activate
+    cd server 
+    python manage.py runserver
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
+import Signup from "./components/signup/Signup";
+import Login from "./components/login/Login";
+import Dashboard from "./components/dashboard/Dashboard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route exact path="/signup" element={<Signup/>}/>
+            <Route exact path="/login" element={<Login/>}/>
+            <Route exact path="/dashboard" element={<Dashboard/>}/>
+            <Route path="*" element={<NotFound/>}/>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
