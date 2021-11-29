432caccb613af748b19929648372cd0e351b3ba6
Maurizio Rendon <maurizio.rendon@system73.com>
2021-11-29 22:57:43 +0000

    feat(BE): Adding CORS allow origins to BE

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -41,8 +41,15 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
+    'corsheaders',
     # 
     'apps.accounts' # add new application
+]
+
+# define which origins are allowed
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
 ]
 
 #configure DRF
@@ -68,6 +75,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'server.urls'
